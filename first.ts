4166596c53998ecdf7b210a91c417d2b354c4e3f
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-16 09:46:14 +0600

    second commit

--- first.ts
+++ first.ts
@@ -1,3 +1,11 @@
-let mrX = 5
-mrX = 50
-mrX = 0
+let mrX: number = 5
+let mrY: string = "50"
+let mrZ: boolean = false
+let color: string = "red"
+
+let fullName: string = "Raufu Prezens"
+let age: number = 21
+let sentence: string = `Hey, it's me ${fullName}
+I'll be ${age+1} next year.
+`
+console.log(sentence)
